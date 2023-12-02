442a903eb4b8b3f113680abab7ac7854fad59c23
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-12-02 08:21:34 +0200

    Calculator Util First init

---
+++ Rune Project/client/src/utils/calculator.js
@@ -0,0 +1,25 @@
+function calculateRunesNeeded(runesArray, targetRune) {
+    // Find the index of the target rune in the array
+    const targetIndex = runesArray.findIndex((rune) => rune.name.toLowerCase() === targetRune.toLowerCase());
+  
+    // If the target rune is not found, return -1 indicating an error
+    if (targetIndex === -1) {
+      console.error(`Error: Rune ${targetRune} not found in the array.`);
+      return -1;
+    }
+  
+    // Calculate the total number of runes needed by multiplying counts in descending order
+    let totalRunesNeeded = 1;
+    for (let i = targetIndex; i < runesArray.length; i++) {
+      totalRunesNeeded *= runesArray[i].neededForCube.count;
+    }
+  
+    return totalRunesNeeded;
+  }
+  
+  // Example usage:
+  const berRunesArray = diablo2RunesArray.slice(0, 29); // Array up to Ber rune
+  const result = calculateRunesNeeded(berRunesArray, 'Zod');
+  
+  console.log(`To cube 1 Zod rune, you need ${result} Ber runes.`);
+  
