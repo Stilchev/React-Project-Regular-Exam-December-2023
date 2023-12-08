1eeb6165a2b82694732ebb676f6ea00b95158b84
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-12-08 18:21:02 +0200

    Logout

--- Rune Project/client/src/components/Logout/Logout.jsx
+++ Rune Project/client/src/components/Logout/Logout.jsx
@@ -1,7 +1,22 @@
-const Logout = () => {
-    return(
-        <h1>Logout</h1>
-    )
-}
+import { useContext, useEffect } from "react";
 
-export default Logout;
+import * as authService from '../../services/authService';
+import { useNavigate } from "react-router-dom";
+
+import AuthContext from "../../contexts/authContext";
+
+export default function Logout() {
+    const navigate = useNavigate();
+    const { logoutHandler } = useContext(AuthContext);
+
+    useEffect(() => {
+        authService.logout()
+            .then(() => {
+                logoutHandler();
+                navigate('/');
+            })
+            .catch(() => navigate('/'));
+    }, []);
+
+    return null;
+}
