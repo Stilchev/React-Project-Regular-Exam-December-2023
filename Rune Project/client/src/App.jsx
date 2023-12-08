6293b4126490c92ad0c95359dce50c0b0cc04ae6
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-12-08 18:20:27 +0200

    Login

--- Rune Project/client/src/App.jsx
+++ Rune Project/client/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
+
+import AuthContext from './contexts/authContext'
+import * as authService from './services/authService'
+
 import Header from './components/Header/Header'
 
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Runes from './components/Runes/Runes';
 import Login from './components/Login/Login'
@@ -13,25 +17,68 @@ import RuneProject from './components/RuneProject/RuneProject'
 import Logout from './components/Logout/Logout'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const navigate = useNavigate()
 
-  return (
-    <>
-      <Header />
+    const [auth, setAuth] = useState(() => {
+        localStorage.removeItem('accessToken');
 
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/runes' element={<Runes />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/register' element={<Register />}/>
-        <Route path='/runes-calculator' element={<RuneCalculator />}/>
-        <Route path='/hrs' element={<HighRunes />}/>
-        <Route path='/runes-project' element={<RuneProject />}/>
-        <Route path='/logout' element={<Logout />}/>
-        <Route path='/*' element={<Home />}/>
-      </Routes>
-    </>
-  )
+        return {};
+    });
+
+    const loginSubmitHandler = async (values) => {
+        const result = await authService.login(values.email, values.password);
+
+        setAuth(result);
+
+        localStorage.setItem('accessToken', result.accessToken);
+
+        navigate('/');
+    }
+    const registerSubmitHandler = async (values) => {
+        const result = await authService.register(values.email, values.password);
+        console.log(result);
+        setAuth(result);
+
+        localStorage.setItem('accessToken', result.accessToken);
+
+        navigate('/');
+    }
+
+    const logoutHandler = () => {
+        setAuth({});
+
+        localStorage.removeItem('accessToken');
+    };
+
+    const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        username: auth.username,
+        email: auth.email,
+        isAuthenticated: !!auth.email,
+    };
+
+    return (
+        <>
+
+            <AuthContext.Provider value={values}>
+                <Header />
+
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/runes' element={<Runes />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/runes-calculator' element={<RuneCalculator />} />
+                    <Route path='/hrs' element={<HighRunes />} />
+                    <Route path='/runes-project' element={<RuneProject />} />
+                    <Route path='/logout' element={<Logout />} />
+                    <Route path='/*' element={<Home />} />
+                </Routes>
+            </AuthContext.Provider>
+        </>
+    )
 }
 
 export default App
--- Rune Project/client/src/components/Login/Login.jsx
+++ Rune Project/client/src/components/Login/Login.jsx
@@ -1,54 +1,122 @@
 // LoginForm.jsx
 import React, { useState } from 'react';
 import './Login.css';
+import useForm from '../../hooks/useForm'
+import { useContext } from 'react';
+import AuthContext from '../../contexts/authContext';
+
+const LoginFormKyes = {
+  Email: 'email',
+  Password: 'password',
+};
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const { loginSubmitHandler } = useContext(AuthContext);
+  const { values, onChange, onSubmit } = useForm(loginSubmitHandler,{
+    [LoginFormKyes.Email]: '',
+    [LoginFormKyes.Password]: '',
+});
 
-  const handleLogin = () => {
-    // Implement your login logic here
-    if (!email || !password) {
-      setError('Please enter both email and password');
-      return;
-    }
+  // const handleLogin = () => {
+  //   // Implement your login logic here
+  //   if (!email || !password) {
+  //     setError('Please enter both email and password');
+  //     return;
+  //   }
 
     // Additional validation logic if needed
 
     // Clear error on successful login
-    setError('');
+    // setError('');
 
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
+    // console.log('Email:', email);
+    // console.log('Password:', password);
+  
 
   return (
     <div className="form-container">
       <h2>Login</h2>
       <form>
-        <label>Email:</label>
+        {/* <label>Email:</label>
         <input
           type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          value={LoginFormKyes.Email}
+          onChange={onChange}
         />
 
         <label>Password:</label>
         <input
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          name="password"
+          value={LoginFormKyes.Password}
+          onChange={onChange}
+        /> */}
 
-        {error && <p className="error-message">{error}</p>}
+        {/* {error && <p className="error-message">{error}</p>} */}
 
-        <button type="button" onClick={handleLogin}>
+        {/* <h1>Login</h1> */}
+             <label htmlFor="email">Email:</label>
+    {/* //         <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" /> */}
+            <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Sokka@gmail.com"
+                onChange={onChange}
+                value={values.email}
+            />
+
+            <label htmlFor="login-pass">Password:</label>
+            {/* <input type="password" id="login-password" name="password" /> */}
+            <input
+                type="password"
+                id="login-password"
+                name="password"
+                onChange={onChange}
+                value={values.password}
+            />
+            {/* <input type="submit" className="btn submit" value="Login" /> */}
+
+         <button type="button" onClick={onSubmit}>
           Login
         </button>
       </form>
     </div>
-  );
-};
 
+    
+    // <form id="login" onSubmit={onSubmit}>
+
+    //     <div className="container">
+    //         <div className="brand-logo"></div>
+    //         <h1>Login</h1>
+    //         <label htmlFor="email">Email:</label>
+    //         <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+    //         <input
+    //             type="email"
+    //             id="email"
+    //             name="email"
+    //             placeholder="Sokka@gmail.com"
+    //             onChange={onChange}
+    //             value={values.email}
+    //         />
+
+    //         <label htmlFor="login-pass">Password:</label>
+    //         <input type="password" id="login-password" name="password" />
+    //         <input
+    //             type="password"
+    //             id="login-password"
+    //             name="password"
+    //             onChange={onChange}
+    //             value={values.password}
+    //         />
+    //         <input type="submit" className="btn submit" value="Login" />
+    //         <p className="field">
+    //             <span>If you don't have profile click <a href="#">here</a></span>
+    //         </p>
+    //     </div>
+    // </form>
+  );
+
+};
 export default Login;
