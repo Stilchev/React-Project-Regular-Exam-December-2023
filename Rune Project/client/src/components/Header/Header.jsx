5c216b183a9e419f5bc8fd443b5c8910109aa29d
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-12-08 18:21:16 +0200

    Conditional Rendering

--- Rune Project/client/src/components/Header/Header.jsx
+++ Rune Project/client/src/components/Header/Header.jsx
@@ -1,41 +1,80 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import { useContext } from 'react';
+import AuthContext from '../../contexts/authContext';
 
 const Header = () => {
-  return (
-    <header className="dark-diablo-header">
-    <div className="header-background"></div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/runes">Runes</Link>
-        </li>
-        <li>
-          <Link to="/runes-calculator">Rune Calculator</Link>
-        </li>
-        <li>
-          <Link to="/runes-project">Rune Project</Link>
-        </li>
-        <li>
-          <Link to="/hrs">High Runes</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-  );
+    const {
+        isAuthenticated,
+        username,
+        email,
+    } = useContext(AuthContext);
+
+    return (
+        <header className="dark-diablo-header">
+            <div className="header-background"></div>
+            <nav>
+                <ul>
+                    {isAuthenticated &&
+                        <>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/runes">Runes</Link>
+                            </li>
+                            <li>
+                                <Link to="/runes-calculator">Rune Calculator</Link>
+                            </li>
+
+                            <li>
+                                <Link to="/hrs">High Runes</Link>
+                            </li>
+                            <li>
+                                <Link to="/runes-project">Rune Project</Link>
+                            </li>
+                            <li>
+                                <Link to="/logout">Logout</Link>
+                                
+                            </li>
+                            <li>
+                                <Link to="#">|| {email} ||</Link>
+                            </li>
+
+                        </>
+                    }
+
+                    {!isAuthenticated &&
+
+                        <>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/runes">Runes</Link>
+                            </li>
+                            <li>
+                                <Link to="/runes-calculator">Rune Calculator</Link>
+                            </li>
+                            <li>
+                                <Link to="/runes-project">Rune Project</Link>
+                            </li>
+                            <li>
+                                <Link to="/hrs">High Runes</Link>
+                            </li>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+
+                            <li>
+                                <Link to="/register">Register</Link>
+                            </li>
+                        </>}
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
