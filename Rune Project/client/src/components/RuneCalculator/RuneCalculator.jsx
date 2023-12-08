6272a4c0d54e25d2a2a68c355a778e6152f61234
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-12-08 10:51:36 +0200

    Add GemsTable

--- Rune Project/client/src/components/RuneCalculator/RuneCalculator.jsx
+++ Rune Project/client/src/components/RuneCalculator/RuneCalculator.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
-import calculateRunesNeeded from '../../utils/calculator';
+// import calculateRunesNeeded from '../../utils/calculator';
 import './RuneCalculator.css'
 import RuneButton from './RuneButton/RuneButton';
 import StartRuneButton from './StartRuneButton/StartRuneButton';
+import GemsTable from './GemsTable/GemsTable';
 // import ResultComponent from './ResultComponent/ResultComponent';
 
 
@@ -42,39 +43,21 @@ const RuneCalculator = () => {
         const finalArray = filteredRunes.slice(startRuneNumber - 1)
         setStartRune(startRuneNumber)
 
-        // const result = calculateRunesNeeded(finalArray)
-        // setResultHandler(result)
         setFinalArray(finalArray)
-        // // console.log(finalArray);
-        // // console.log(result);
-        // console.log(finalResult);
+        
         let totalRunesNeeded = 1;
         for (let i = finalArray.length - 1; i >= 0; i--) {
             
             totalRunesNeeded *= finalArray[i].count;
-            // finalArray[i].gems = totalRunesNeeded
-            //   runesArray[i].dropped = totalRunesNeeded;
-            
+          
         }
-        // console.log(totalRunesNeeded);
         setFinalResult(totalRunesNeeded)
         setTargetRune(0)
-        // return totalRunesNeeded;
         setResult((state)=>({...state,startRuneName:name}))
         setResult((state)=>({...state,result:totalRunesNeeded}))
 
     }
-    // console.log(finalArray[0]);
-    // console.log(finalArray[finalArray.length-1]);
-    // const clculatorHandler = () => {
-    //     calculateRunesNeeded()
-    // }
-    // const one = finalArray[0];
-    // const two = finalArray[finalArray.length-1];
     
-    // console.log(one);
-    // console.log(two);
-    console.log(result);
     return (
         <>
             {!targetRune &&
@@ -107,12 +90,7 @@ const RuneCalculator = () => {
 
                 </div>
             }
-            {/* {startName &&<ResultComponent 
-                startName={one.name}
-                targetName={two.name}
-                totalRunesNeeded={totalRunesNeeded}
-                />} */}
-                
+            
                 {result.result &&
                 <>
                 <table className='calc-result'>
@@ -131,84 +109,9 @@ const RuneCalculator = () => {
                         </tr>
                     </tbody>
                 </table>
-                {/* <p className='calc-result'>Target:{result.targetRuneName}</p>
-                <p className='calc-result'>Start:{result.startRuneName}</p>
-                <p className='calc-result'>Result{result.result}</p> */}
                 </>
                 }
-                
-
-            <div className="table-container">
-                <table className="calculator-gem-table">
-                    <thead>
-                        <tr>
-                            <th colSpan={12}>Required Gems</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="calculator-gem-table">
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\chipped_topaz.png" alt="Chipped Topaz" /><span>Chipped Topaz</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\chipped_amethyst.png" alt="Chipped Amethyst" /><span>Chipped Amethyst</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\chipped_saphire.png" alt="Chipped Sapphire" /><span>Chipped Sapphire</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\chipped_ruby.png" alt="Chipped Ruby" /><span>Chipped Ruby</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\chipped_emerald.png" alt="Chipped Emerald" /><span>Chipped Emerald</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\chipped_diamond.png" alt="Chipped Diamond" /><span>Chipped Diamond</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawed_topaz.png" alt="Flawed Topaz" /><span>Flawed Topaz</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawed_amethyst.png" alt="Flawed Amethyst" /><span>Flawed Amethyst</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawed_saphire.png" alt="Flawed Sapphire" /><span>Flawed Sapphire</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawed_ruby.png" alt="Flawed Ruby" /><span>Flawed Ruby</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawed_emerald.png" alt="Flawed Emerald" /><span>Flawed Emerald</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawed_diamond.png" alt="Flawed Diamond" /><span>Flawed Diamond</span></td>
-
-
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                            <td>5</td>
-                            <td>6</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>9</td>
-                            <td>10</td>
-                            <td>11</td>
-                            <td>12</td>
-                        </tr>
-                        <tr className="calculator-gem-table">
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\topaz.png" alt="Topaz" /><span>Topaz</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\amethyst.png" alt="Amethyst" /><span>Amethyst</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\saphire.png" alt="Sapphire" /><span>Sapphire</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\ruby.png" alt="Ruby" /><span>Ruby</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\emerald.png" alt="Emerald" /><span>Emerald</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\diamond.png" alt="Diamond" /><span>Diamond</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawless_topaz.png" alt="Flawless Topaz" /><span>Flawless Topaz</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawless_amethyst.png" alt="Flawless Amethyst" /><span>Flawless Amethyst</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawless_saphire.png" alt="Flawless Sapphire" /><span>Flawless Sapphire</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawless_ruby.png" alt="Flawless Ruby" /><span>Flawless Ruby</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="\images\gemImages\flawless_emerald.png" alt="Flawless Emerald" /><span>Flawless Emerald</span></td>
-                            <td className="calculator-gem-table"> <img className='gem' src="" /><span>Total Gems</span></td>
-
-
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                            <td>5</td>
-                            <td>6</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>9</td>
-                            <td>10</td>
-                            <td>11</td>
-                            <td>12</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            <GemsTable />
         </>
 
     )
