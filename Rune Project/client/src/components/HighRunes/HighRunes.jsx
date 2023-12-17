e9998d7704a390e002222db7815540f448466f4c
Stilchev <114847091+Stilchev@users.noreply.github.com>
2023-12-17 22:21:20 +0200

    High runes links

--- Rune Project/client/src/components/HighRunes/HighRunes.jsx
+++ Rune Project/client/src/components/HighRunes/HighRunes.jsx
@@ -1,6 +1,28 @@
+import { Link } from "react-router-dom";
+
 const HighRunes = () => {
     return(
-        <h1>High runes</h1>
+        <div>
+      <Link to="/all-high-runes-list" style={{ textDecoration: 'none' }}>
+        <button style={{ fontSize: '24px', margin: '10px' }}>
+          All High Runes List
+        </button>
+      </Link>
+
+      <Link to="/your-high-runes" style={{ textDecoration: 'none' }}>
+        <button style={{ fontSize: '24px', margin: '10px' }}>
+          Your High Runes
+        </button>
+      </Link>
+
+      <Link to="/add-new-high-rune" style={{ textDecoration: 'none' }}>
+        <button style={{ fontSize: '24px', margin: '10px' }}>
+          Add New High Rune
+        </button>
+      </Link>
+    </div>
+  
+
     )
 }
 
